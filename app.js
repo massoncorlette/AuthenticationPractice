398b8adcdfcc29c752b21f2a7df1d423466d91ee
massoncorlette <massoncorlette07@gmail.com>
2025-04-20 03:13:04 -0400

    adding a signup view and a route to it

--- app.js
+++ app.js
@@ -25,4 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("index"));
 
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+
 app.listen(3000, () => console.log("app listening on port 3000!"));
