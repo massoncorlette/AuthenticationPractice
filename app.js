f10fa7b1a442cbed01cb6e9d783945b1f4b3c8a4
massoncorlette <massoncorlette07@gmail.com>
2025-04-20 04:36:06 -0400

    update commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /////// app.js
 
+require('dotenv').config();
 const path = require("node:path");
 const { Pool } = require("pg");
 const express = require("express");
@@ -26,6 +27,18 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => res.render("index"));
 
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch(err) {
+    return next(err);
+  }
+});
 
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
