24725a680eae8acff3e41c10240ba4edfbccc066
massoncorlette <massoncorlette07@gmail.com>
2025-04-24 00:25:45 -0400

    adding bycrypt.compare to compare login password with hashed password in db

--- app.js
+++ app.js
@@ -70,9 +70,14 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
+
+      //comparing login password to hashed stored password
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" })
       }
+      
       return done(null, user);
     } catch(err) {
       return done(err);
