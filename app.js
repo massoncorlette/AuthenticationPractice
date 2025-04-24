f1a38eb2173fd65d2e9fab9c10ae4425c61f3196
massoncorlette <massoncorlette07@gmail.com>
2025-04-23 23:03:36 -0400

    adding passport methods to save session user login + keep user logged in upon traversing the application using 'done' callback function specified in LocalStrategy

--- app.js
+++ app.js
@@ -61,5 +61,21 @@ passport.use(
 );
 
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch(err) {
+    done(err);
+  }
+});
+
+
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
